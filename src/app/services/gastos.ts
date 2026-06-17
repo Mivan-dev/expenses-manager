fa5afaf902e303147e08cfb0014d25c7fb93c070
Ivan-M <sirius.ivo@gmail.com>
2026-06-17 03:43:58 -0300

    refactor: update Gastos class to GastosService and implement signal for state management

--- src/app/services/gastos.ts
+++ src/app/services/gastos.ts
@@ -1,8 +1,35 @@
-import { Injectable } from '@angular/core';
+import { Injectable, signal } from '@angular/core';
+import { Tarjeta } from '../models/tarjeta.model';
+import { Servicio } from '../models/servicio.model';
 
 @Injectable({
   providedIn: 'root',
 })
-export class Gastos {
+export class GastosService {
+  tarjeta = signal<Tarjeta[]>([])
+  servicio = signal<Servicio[]>([])
   
+  constructor() {
+    
+    this.loadData();
+    //this.saveData();
+    
+  }
+
+  private loadData() {
+    const tarjetasData = localStorage.getItem('tarjetas');
+    const serviciosData = localStorage.getItem('servicios');
+    if (tarjetasData) {
+      this.tarjeta.set(JSON.parse(tarjetasData));
+    }
+    if (serviciosData) {
+      this.servicio.set(JSON.parse(serviciosData));
+    }
+  }
+
+  private saveData() {
+    localStorage.setItem('tarjetas', JSON.stringify(this.tarjeta()));
+    localStorage.setItem('servicios', JSON.stringify(this.servicio()));
+  }
+
 }
