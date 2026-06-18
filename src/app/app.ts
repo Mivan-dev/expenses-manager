0b90ea2533262e05a9eed89f7790ad6c58ce60c0
Ivan-M <sirius.ivo@gmail.com>
2026-06-18 04:42:37 -0300

    feat: integrating nav, footer, and tarjetas components

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Nav } from "./components/layout/nav/nav";
+import { Footer } from "./components/layout/footer/footer";
+import { Tarjetas } from "./components/features/tarjetas/tarjetas";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Nav, Footer, Tarjetas],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
