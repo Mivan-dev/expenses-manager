c80b7db690887f3b9b471c0186342c36340b9508
Ivan-M <sirius.ivo@gmail.com>
2026-06-24 03:39:46 -0300

    feat: enhance tarjetas component with improved layout and currency formatting

--- src/app/components/features/tarjetas/tarjetas.ts
+++ src/app/components/features/tarjetas/tarjetas.ts
@@ -1,14 +1,17 @@
 import { Component, inject } from '@angular/core';
 import { GastosService } from '../../../services/gastos';
+import { CurrencyPipe, DatePipe } from '@angular/common';
+import { registerLocaleData } from '@angular/common';
+import localeEsAR from '@angular/common/locales/es-AR';
+
+registerLocaleData(localeEsAR);
 
 @Component({
   selector: 'app-tarjetas',
-  imports: [],
+  imports: [CurrencyPipe, DatePipe],
   templateUrl: './tarjetas.html',
   styleUrl: './tarjetas.css',
 })
 export class Tarjetas {
-
-  gastosService = inject(GastosService)
-
+  gastosService = inject(GastosService);
 }
