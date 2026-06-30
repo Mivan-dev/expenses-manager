d65fdcd0e878e46bbd49c5b572ae7fb8a51ce6ca
Ivan-M <sirius.ivo@gmail.com>
2026-06-30 01:15:31 -0300

    feat: add edit button with icon and update imports for icon support in tarjetas component

--- src/app/components/features/tarjetas/tarjetas.ts
+++ src/app/components/features/tarjetas/tarjetas.ts
@@ -3,12 +3,15 @@ import { GastosService } from '../../../services/gastos';
 import { CurrencyPipe, DatePipe } from '@angular/common';
 import { registerLocaleData } from '@angular/common';
 import localeEsAR from '@angular/common/locales/es-AR';
+import { NgIconComponent, provideIcons } from '@ng-icons/core';
+import { heroTrash, heroPencil, heroPlus } from '@ng-icons/heroicons/outline';
 
 registerLocaleData(localeEsAR);
 
 @Component({
   selector: 'app-tarjetas',
-  imports: [CurrencyPipe, DatePipe],
+  imports: [CurrencyPipe, DatePipe, NgIconComponent],
+  providers: [provideIcons({ heroTrash, heroPencil, heroPlus })],
   templateUrl: './tarjetas.html',
   styleUrl: './tarjetas.css',
 })
