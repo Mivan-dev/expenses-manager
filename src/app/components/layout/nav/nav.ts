1d5900d7daa821ce1a99f70dda866c52367d50ab
Ivan-M <sirius.ivo@gmail.com>
2026-07-14 23:49:48 -0300

    feat: enhance navigation component with icons and improved button styles

--- src/app/components/layout/nav/nav.ts
+++ src/app/components/layout/nav/nav.ts
@@ -1,27 +1,27 @@
 import { Component, EventEmitter, inject, Output, signal } from '@angular/core';
 import { GastosService } from '../../../services/gastos';
+import { NgIconComponent, provideIcons } from '@ng-icons/core';
+import { heroCreditCard, heroDocumentCurrencyDollar, heroPlus, heroArrowLeftStartOnRectangle, heroArrowsRightLeft } from '@ng-icons/heroicons/outline';
 
 @Component({
   selector: 'app-nav',
-  imports: [],
+  imports: [NgIconComponent],
+  providers: [provideIcons({ heroCreditCard, heroDocumentCurrencyDollar, heroPlus, heroArrowLeftStartOnRectangle, heroArrowsRightLeft })],
   templateUrl: './nav.html',
   styleUrl: './nav.css',
 })
 export class Nav {
+  modal = inject(GastosService);
+  isExpanded = signal<boolean>(true);
 
-  modal = inject(GastosService)
-  isExpanded = signal<boolean>(true)
-
-  toggle(){
-    this.isExpanded.set(!this.isExpanded())
-    this.expandedChange.emit(this.isExpanded()) 
+  toggle() {
+    this.isExpanded.set(!this.isExpanded());
+    this.expandedChange.emit(this.isExpanded());
   }
 
-  abrirModal(){
-    this.modal.abrirModal("tarjeta")
+  abrirModal() {
+    this.modal.abrirModal('tarjeta');
   }
 
-  @Output() expandedChange = new EventEmitter<boolean>()
-
-
+  @Output() expandedChange = new EventEmitter<boolean>();
 }
