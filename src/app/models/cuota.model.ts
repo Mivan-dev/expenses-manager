878fbe13f9a4aa7ef0668197ef3e6533257c5841
Ivan-M <sirius.ivo@gmail.com>
2026-07-30 23:32:28 -0300

    fix: add cuotaBase field to Cuota model

--- src/app/models/cuota.model.ts
+++ src/app/models/cuota.model.ts
@@ -3,6 +3,7 @@ import { EntidadBase } from "./base.model";
 export interface Cuota extends EntidadBase {
     cuotaActual: number;
     cuotaTotal: number;
+    cuotaBase: number;
     monto: number;
     fechaCarga: string;
 }
