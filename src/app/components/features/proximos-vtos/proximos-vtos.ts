41cd79615b2a4f7a96253820084cdf6909bfa4d9
Ivan-M <sirius.ivo@gmail.com>
2026-08-15 21:10:34 -0300

    feat: implement ProximosVtos component to display upcoming payments with dynamic styling

--- src/app/components/features/proximos-vtos/proximos-vtos.ts
+++ src/app/components/features/proximos-vtos/proximos-vtos.ts
@@ -1,11 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { GastosService } from '../../../services/gastos';
+import { CurrencyPipe, DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-proximos-vtos',
-  imports: [],
+  imports: [DatePipe, CurrencyPipe],
   templateUrl: './proximos-vtos.html',
   styleUrl: './proximos-vtos.css',
 })
 export class ProximosVtos {
+  gastosService = inject(GastosService)
 
+  diasFaltantes(fecha: Date): number {
+    const hoy = new Date();
+    return Math.round((fecha.getTime() - hoy.getTime()) / 86400000);
+  }
 }
