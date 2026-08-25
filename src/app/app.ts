fb3407f31865a9abc7eca50acf48e1c1e51706f5
Ivan-M <sirius.ivo@gmail.com>
2026-08-24 21:37:56 -0300

    refactor: simplify App component by removing unused imports and HTML structure

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,34 +1,11 @@
-import { Component, inject, signal } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { Nav } from "./components/layout/nav/nav";
-import { Footer } from "./components/layout/footer/footer";
-import { Tarjetas } from "./components/features/tarjetas/tarjetas";
-import { CurrencyPipe, NgClass } from '@angular/common';
-import { GastosService } from './services/gastos';
-import { TarjetaForm } from "./components/features/tarjeta-form/tarjeta-form";
-import { CuotaForm } from "./components/features/cuota-form/cuota-form";
-import { ConfirmModal } from './components/features/confirm-modal/confirm-modal';
-import { ServicioForm } from './components/features/servicio-form/servicio-form';
-import { Servicios } from './components/features/servicios/servicios';
-import { ProximosVtos } from "./components/features/proximos-vtos/proximos-vtos";
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Nav, Footer, Tarjetas, NgClass, TarjetaForm, CuotaForm, ConfirmModal, ServicioForm, Servicios, CurrencyPipe, ProximosVtos],
+  imports: [RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
-  protected readonly title = signal('expenses-manager');
-
-  constructor(){
-    this.gastosService.actualizarCuotas();
-  }
-
-  isNavExpanded = signal<boolean>(true)
-  gastosService = inject(GastosService)
-
-  onNavToggle(value: boolean){
-    this.isNavExpanded.set(value)
-  }
-}
+export class App {}
