bf9550f3e1ab9bca4166be45853fded9009198ee
Ivan-M <sirius.ivo@gmail.com>
2026-08-24 21:38:02 -0300

    feat: define application routes with dashboard and login components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
+import { Auth } from './services/auth';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        loadComponent: () => import('./pages/dashboard/dashboard').then(m => m.Dashboard),
+        canActivate: [Auth]
+    },
+    {
+        path: 'login',
+        loadComponent: () => import('./pages/login/login').then(m => m.Login)
+    },
+    {
+        path: '**',
+        loadComponent: () => import('./pages/login/login').then(m => m.Login)
+    }
+];
