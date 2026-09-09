b45778cbae1336f12f8bbca721fc9664782f45e6
IvanM <sirius.ivo@gmail.com>
2026-09-09 04:28:53 -0300

    feat: add etiqueta1, valor1, etiqueta2, and valor2 properties to Servicio interface

--- src/app/models/servicio.model.ts
+++ src/app/models/servicio.model.ts
@@ -4,4 +4,26 @@ export interface Servicio extends EntidadBase {
     empresaId: string;
     monto: number;
     vencimiento: string;
+    etiqueta1?: EtiquetaCredencial;
+    valor1?: string;
+    etiqueta2?: EtiquetaCredencial;
+    valor2?: string
+}
+
+export enum EtiquetaCredencial {
+    NUMERO_CLIENTE = 'NUMERO_CLIENTE',
+    NUMERO_CONTRATO = 'NUMERO_CONTRATO',
+    NUMERO_CUENTA = 'NUMERO_CUENTA',
+    UF = 'UF',
+    NUMERO = 'NUMERO',
+    IDENTIFICADOR = 'IDENTIFICADOR',
+}
+
+export enum EtiquetaLabels {
+    NUMERO_CLIENTE = 'N° Cliente',
+    NUMERO_CONTRATO = 'N° Contrato',
+    NUMERO_CUENTA = 'N° Cuenta',
+    UF = 'UF',
+    NUMERO = 'Número',
+    IDENTIFICADOR = 'Identificador',
 }
