b23944b4fb3a05007f3ad95ce8eb7eff11f615a7
IvanM <sirius.ivo@gmail.com>
2026-09-09 13:05:11 -0300

    feat: enhance confirm modal and credencial form with improved validation and UI updates

--- src/app/components/features/confirm-modal/confirm-modal.ts
+++ src/app/components/features/confirm-modal/confirm-modal.ts
@@ -24,7 +24,34 @@ export class ConfirmModal {
     } 
     else if (tipo === 'confirmarServicio'){
     this.gastosService.eliminarServicio(idElemento!)
-  }
-  this.gastosService.cerrarModal();
+    }
+    else if (tipo === 'confirmarCredencial'){
+      const servicio = this.gastosService.servicioEditando()!
+      const numCredencial = this.gastosService.credencialSeleccionada()
+      if(numCredencial === '1'){
+        this.gastosService.editarServicio(idElemento!, {
+        nombre: servicio.nombre,
+        monto: servicio.monto,
+        vencimiento: servicio.vencimiento,
+        empresaId: servicio.empresaId,
+        etiqueta1: null,
+        valor1: null,
+        etiqueta2: servicio.etiqueta2,
+        valor2: servicio.valor2
+      })
+      } else {
+        this.gastosService.editarServicio(idElemento!, {
+        nombre: servicio.nombre,
+        monto: servicio.monto,
+        vencimiento: servicio.vencimiento,
+        empresaId: servicio.empresaId,
+        etiqueta1: servicio.etiqueta1,
+        valor1: servicio.valor1,
+        etiqueta2: null,
+        valor2: null
+      });
+      }
+    }
+    this.gastosService.cerrarModal();
   }
 }
--- src/app/components/features/credencial-form/credencial-form.ts
+++ src/app/components/features/credencial-form/credencial-form.ts
@@ -12,16 +12,16 @@ import { EtiquetaCredencial, EtiquetaLabels } from '../../../models/servicio.mod
 export class CredencialForm {
 
   constructor(){
-    const credenciales = this.gastosService.servicioEditando();
-    if (credenciales?.etiqueta1 && credenciales.valor1){
-      this.form.patchValue({
-        etiqueta1: credenciales.etiqueta1,
-        valor1: credenciales.valor1,
-        etiqueta2: credenciales.etiqueta2,
-        valor2: credenciales.valor2
-      })
-    }
+  const credenciales = this.gastosService.servicioEditando();
+  if (credenciales?.etiqueta1 || credenciales?.etiqueta2){
+    this.form.patchValue({
+      etiqueta1: credenciales.etiqueta1,
+      valor1: credenciales.valor1,
+      etiqueta2: credenciales.etiqueta2,
+      valor2: credenciales.valor2
+    })
   }
+}
 
   etiquetas = Object.values(EtiquetaCredencial)
   etiquetaLabels = EtiquetaLabels
@@ -37,17 +37,19 @@ export class CredencialForm {
 
   onSubmit(){
     const credencialesEditando = this.gastosService.servicioEditando();
+    const credencial1Valida = !!this.form.value.etiqueta1 && !!this.form.value.valor1;
+    const credencial2Valida = !!this.form.value.etiqueta2 && !!this.form.value.valor2;
     if (this.form.valid){
-      if(credencialesEditando){
+      if(credencialesEditando && (credencial1Valida || credencial2Valida)){
         const editandoCredenciales = {
           nombre: credencialesEditando.nombre,
           monto: credencialesEditando.monto,
           vencimiento: credencialesEditando.vencimiento,
           empresaId: credencialesEditando.empresaId,
-          etiqueta1: this.form.value.etiqueta1! as EtiquetaCredencial,
-          valor1: this.form.value.valor1!,
-          etiqueta2: this.form.value.etiqueta2! as EtiquetaCredencial,
-           valor2: this.form.value.valor2!,
+          etiqueta1: this.form.value.etiqueta1 as EtiquetaCredencial || undefined,
+          valor1: this.form.value.valor1 || undefined,
+          etiqueta2: this.form.value.etiqueta2 as EtiquetaCredencial || undefined,
+          valor2: this.form.value.valor2 || undefined,
         }
         this.gastosService.editarServicio(credencialesEditando.id, editandoCredenciales)
       }
