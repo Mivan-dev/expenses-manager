42a379ae26a435702965f477fadb61f623d7d9e1
IvanM <sirius.ivo@gmail.com>
2026-09-09 04:28:58 -0300

    refactor: improve formatting and add missing properties to update method in ServicioApi

--- src/app/services/servicio-api/servicio-api.ts
+++ src/app/services/servicio-api/servicio-api.ts
@@ -1,31 +1,43 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
-import { Servicio } from '../../models/servicio.model';
+import { EtiquetaCredencial, Servicio } from '../../models/servicio.model';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ServicioApi {
-    httpClient = inject(HttpClient);
+  httpClient = inject(HttpClient);
   baseURL: string = 'http://localhost:3000';
 
-  getAll(){
-    return this.httpClient.get<Servicio[]>(this.baseURL+'/servicio')
+  getAll() {
+    return this.httpClient.get<Servicio[]>(this.baseURL + '/servicio');
   }
 
-  getOne(id: string){
-    return this.httpClient.get<Servicio>(this.baseURL+'/servicio/'+id)
+  getOne(id: string) {
+    return this.httpClient.get<Servicio>(this.baseURL + '/servicio/' + id);
   }
 
-  create(data: {nombre: string, empresaId: string, monto: number, vencimiento: string}){
-    return this.httpClient.post<Servicio>(this.baseURL+'/servicio', data)
+  create(data: { nombre: string; empresaId: string; monto: number; vencimiento: string }) {
+    return this.httpClient.post<Servicio>(this.baseURL + '/servicio', data);
   }
 
-  update(id: string, data: {nombre: string, empresaId: string, monto: number, vencimiento: string}){
-    return this.httpClient.patch<Servicio>(this.baseURL+'/servicio/'+id, data)
+  update(
+    id: string,
+    data: {
+      nombre: string;
+      empresaId: string;
+      monto: number;
+      vencimiento: string;
+      etiqueta1?: EtiquetaCredencial;
+      valor1?: string;
+      etiqueta2?: EtiquetaCredencial;
+      valor2?: string;
+    },
+  ) {
+    return this.httpClient.patch<Servicio>(this.baseURL + '/servicio/' + id, data);
   }
 
-  delete(id:string){
-    return this.httpClient.delete(this.baseURL+'/servicio/'+id)
+  delete(id: string) {
+    return this.httpClient.delete(this.baseURL + '/servicio/' + id);
   }
 }
